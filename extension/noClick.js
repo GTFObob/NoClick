4250eb98877f234bf3636ef4163859c149ac3b15
sakshaat_dc <sakshaat_dc@hotmail.com>
2017-05-11 16:52:56 -0400

    Browser context taken out, templating added in the backend

--- extension/noClick.js
+++ extension/noClick.js
@@ -1,7 +1,7 @@
 // initializes the contextmenu on installation.
 chrome.runtime.onInstalled.addListener(function() {
 	chrome.contextMenus.create({
-	    "title": "NoClick",
+	    "title": "NoClick: Get a gist",
 	    "id": 'noClick',
 	    "contexts": ["link"],
 	  });
@@ -10,7 +10,13 @@ chrome.runtime.onInstalled.addListener(function() {
 // the listener function, run when someone right clicks the extension's menu in contextmenu.
 function contextClicked(info, tab) {
     if (info.menuItemId === "noClick") {
-    	articleTopic();
+    	if(!info.linkUrl) return;
+
+    	url = 'http://noclick-api.jrtzicmdwr.us-east-2.elasticbeanstalk.com/summarize';
+    	url += "?url=" + info.linkUrl;
+    	
+    	chrome.windows.create({url: url, type: 'popup', height: 600, width: 600}, 
+            function(window) {});
     }
 }
 
