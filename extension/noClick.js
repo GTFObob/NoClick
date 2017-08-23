004c22808b59486280ef176852ab65a9864465d2
sakshaat_dc <sakshaat_dc@hotmail.com>
2017-08-23 17:08:28 -0400

    Heroku test

--- extension/noClick.js
+++ extension/noClick.js
@@ -1,28 +1,29 @@
 // initializes the contextmenu on installation.
 chrome.runtime.onInstalled.addListener(function() {
-	chrome.contextMenus.create({
-	    "title": "NoClick: Get a gist",
-	    "id": 'noClick',
-	    "contexts": ["link"],
-	  });
+    chrome.contextMenus.create({
+        "title": "NoClick: Get a gist",
+        "id": 'noClick',
+        "contexts": ["link"],
+    });
 });
 
 // the listener function, run when someone right clicks the extension's menu in contextmenu.
 function contextClicked(info, tab) {
     if (info.menuItemId === "noClick") {
-    	if(!info.linkUrl) return;
+        if (!info.linkUrl) return;
 
-    	url = 'http://noclick-api.jrtzicmdwr.us-east-2.elasticbeanstalk.com/summarize';
-    	url += "?url=" + info.linkUrl + "&num=3";
-    	
-    	chrome.windows.create({url: url, type: 'popup', height: 600, width: 600}, 
-            function(window) {});
+        url = 'http://noclick-api.jrtzicmdwr.us-east-2.elasticbeanstalk.com/summarize';
+        url += "?url=" + info.linkUrl + "&num=3";
+
+        chrome.tabs.executeScript(tab.id, {
+            file: "script.js"
+        }, function() {})
     }
 }
 
 // Used to render stuff onto the pop up page.
 function renderStatus(statusText) {
-  document.getElementById('status').textContent = statusText;
+    document.getElementById('status').textContent = statusText;
 }
 
 // calls the listener.
