d0fc935132a22a4b1c24b5b21a16508eee9bbc82
sakshaat_dc <sakshaat_dc@hotmail.com>
2017-08-23 18:44:31 -0400

    splitting the two

--- Dockerfile
+++
@@ -1,4 +0,0 @@
-FROM python:3-onbuild
-EXPOSE 5000
-RUN python -m nltk.downloader punkt
-CMD ["python", "./application.py"]
--- backend/wsgi.py
+++
@@ -1,6 +0,0 @@
-from application import application
-import nltk
-
-if __name__ == "__main__":
-    nltk.download('punkt')
-    application.run()
