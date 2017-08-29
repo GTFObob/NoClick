73a34996d078137aaafb0c2a07d070ce72d71c33
sakshaat_dc <sakshaat_dc@hotmail.com>
2017-08-29 02:22:33 -0400

    Fixing an issue with SSL certificates

--- static/script.js
+++ static/script.js
@@ -10,32 +10,43 @@ function update_display(elements) {
         res_ul.append('<li>' + element + '</li>')
     })
 
-    $("#response").removeClass("disabled");
+    $("#response").removeClass("loading");
 }
 
 $(function() {
     $("#search-button").click(function() {
         req = { url: $("#search-input").val() }
-        $.get('/summarize', req, function(res) {
-            res = JSON.parse(res)
-            update_display(res);
 
-        }).fail(function(res) {
-            console.log(res);
-            var message;
-            switch (res.status) {
-                case 403:
-                case 400:
-                    message = "Please enter a valid URL and try again.";
-                    break;
-                default:
-                    message = "An unknown error occurred."
+        $.get({
+            url: '/summarize',
+            data: req,
+            dataType: "json",
+            beforeSend: function() {
+                $("#response").removeClass("disabled");
+                $("#response").addClass("loading");
+            },
+            success: function(res) {
+                update_display(res);
+            },
+            error: function(res) {
+                var message;
+                switch (res.status) {
+                    case 403:
+                    case 400:
+                        message = "Please enter a valid URL and try again.";
+                        break;
+                    default:
+                        message = "An unknown error occurred."
+                }
+
+                $("#response").removeClass("loading");
+                $("#response").addClass("disabled");
+
+                $(".content .message").html(message);
+                $('.page.dimmer:first')
+                    .dimmer('toggle');
+
             }
-
-            $(".content .message").html(message);
-            $('.page.dimmer:first')
-                .dimmer('toggle');
         })
-
     })
 });
