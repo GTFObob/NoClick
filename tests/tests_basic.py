a5280cdc1e29a79495818a77b1cd95ed8ce809a1
sakshaat_dc <sakshaat_dc@hotmail.com>
2017-08-30 13:02:57 -0400

    Adding basic tests and circle.yml

---
+++ tests/tests_basic.py
@@ -0,0 +1,18 @@
+import unittest
+from app import app
+
+class BasicTests(unittest.TestCase):
+    def setUp(self):
+        self.app = app.test_client()
+        self.app.testing = True
+    
+    def tearDown(self):
+        pass
+    
+    def test_home_status_code(self):
+        result = self.app.get("/")
+        self.assertEqual(result.status_code, 200)
+
+# runs the unit tests in the module
+if __name__ == '__main__':
+  unittest.main()
